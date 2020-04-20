1fc893265f507c76ce099df7f3e115485c79881b
Choong Jin Ng <jin8383@gmail.com>
2020-04-20 14:19:40 -0700

    Removed birth and death metrics from model. Fixed plot colours

--- seirdp.py
+++ seirdp.py
@@ -9,7 +9,7 @@ import math
 import covid_params # Change the values here if you want to adjust one variable at a time
 
 class seirdp():
-  def __init__(self, r0, r1, gamma, sigma, baseAlpha, fatalityRateAvg, birthRateDaily=0.0, natDeathRateDaily=0.0, socDistResponseFactor=1.0, diseaseScalingFactor=0.0):
+  def __init__(self, r0, r1, gamma, sigma, baseAlpha, fatalityRateAvg, socDistResponseFactor=1.0, diseaseScalingFactor=0.1):
     # Assign variables
     self.r0 = r0
     self.r1 = r1
@@ -17,8 +17,6 @@ class seirdp():
     self.sigma = sigma
     self.baseAlpha = baseAlpha
     self.pho = fatalityRateAvg
-    self.birthRateDaily = birthRateDaily
-    self.natDeathRateDaily = natDeathRateDaily
     self.socDistResponseFactor = socDistResponseFactor
     self.diseaseScalingFactor = diseaseScalingFactor
 
@@ -27,7 +25,7 @@ class seirdp():
 
   ## Model of SEIRDP
   def model(self, Y, x, N, r0, thrPop, thrDay, r1, gamma, sigma, pho):
-    S, E, I, R, D, P = Y
+    S, E, I, R, D = Y
 
     # Disease population hits threshold. Begin social distancing
     if ((I + R + D) > thrPop and x < thrDay):
@@ -42,44 +40,43 @@ class seirdp():
         beta = self.logisticFunction(x, thrDay, self.socDistResponseFactor) * gamma
     else:
       beta = r0*gamma
-    alpha = self.fatalityRate(I, N)
+    alpha = self.alphaGenerated(I, N)
 
-    dS = - beta * S * I / N + self.birthRateDaily * N - self.natDeathRateDaily * S
-    dE = beta * S * I / N - sigma * E + self.birthRateDaily * E
-    dI = sigma * E - (1 - alpha) * gamma * I - alpha * pho * I + self.birthRateDaily * I
-    dR = (1 - alpha)*gamma * I - self.natDeathRateDaily * R + self.birthRateDaily * N
+    dS = - beta * S * I / N
+    dE = beta * S * I / N - sigma * E
+    dI = sigma * E - (1 - alpha) * gamma * I - alpha * pho * I
+    dR = (1 - alpha)*gamma * I
     dD = alpha*pho*I
-    dP = self.natDeathRateDaily* (S + R)
-    return dS, dE, dI, dR, dD, dP
+    return dS, dE, dI, dR, dD
 
   ## Modelling R0 from before and after countermeasures
   def logisticFunction(self, x, x0, k):
     return (self.r0 - self.r1)/(1 + math.exp(-k*(-x + x0))) + self.r1
 
   ## Fatality Rate bases the kill rate based on 
-  def fatalityRate(self, I, N):
+  def alphaGenerated(self, I, N):
     overworkedAlpha = self.diseaseScalingFactor * I / N # If the population is not able to cope, this comes into play
     return overworkedAlpha + self.baseAlpha
 
   ## Getting the populations of the different states
   def solve(self, population, E0, thrPop, thrDay, daysModel):
     X = np.arange(daysModel)  # time steps array
-    N0 = population - E0, E0, 0, 0, 0, 0  # S, E, I, R, D at initial step
+    N0 = population - E0, E0, 0, 0, 0  # S, E, I, R at initial step
     self.thresholdPrintFlag = True
     
     y_data_var = scipy.integrate.odeint(self.model, N0, X, args=(
       population, self.r0, thrPop, thrDay, self.r1, self.gamma, self.sigma, self.pho))
       
-    S, E, I, R, D, P = y_data_var.T  # transpose and unpack
-    return X, S, E, I, R, D, P  # note these are all arrays
+    S, E, I, R, D = y_data_var.T  # transpose and unpack
+    return X, S, E, I, R, D  # note these are all arrays
 
 ## If this is the main python file...
 if __name__ == "__main__":
   E0 = 1  # exposed at initial time step
 
-  modelInstance = seirdp(covid_params.r0, covid_params.r1, covid_params.GAMMA, covid_params.SIGMA, covid_params.BASE_ALPHA_RATE, covid_params.FATALITY_RATE_AVERAGE,
-    covid_params.BIRTH_RATE_ANNUAL/covid_params.NUM_DAYS_IN_YEAR, covid_params.NATURAL_DEATH_RATE_ANNUAL/covid_params.NUM_DAYS_IN_YEAR, covid_params.SOCIAL_DISTANCE_RESPONSE_FACTOR)
-  X, S, E, I, R, D, P = modelInstance.solve(covid_params.POPULATION, E0, covid_params.SOCIAL_DISTANCE_THRESHOLD_POPULATION, covid_params.SOCIAL_DISTANCE_DAY, covid_params.DAYS_MODEL)
+  modelInstance = seirdp(covid_params.r0, covid_params.r1, covid_params.GAMMA, covid_params.SIGMA,
+    covid_params.KILL_PROBABILITY, covid_params.FATALITY_RATE_AVERAGE, covid_params.SOCIAL_DISTANCE_RESPONSE_FACTOR)
+  X, S, E, I, R, D = modelInstance.solve(covid_params.POPULATION, E0, covid_params.SOCIAL_DISTANCE_THRESHOLD_POPULATION, covid_params.SOCIAL_DISTANCE_DAY, covid_params.DAYS_MODEL)
 
   # Plot
   fig = plt.figure(dpi=75, figsize=(20,16))
@@ -87,9 +84,8 @@ if __name__ == "__main__":
   ax.plot(X, S, 'o', color='orange', label='Susceptible')
   ax.plot(X, E, 'y', alpha=0.5, lw=2, label='Exposed (realtime)')
   ax.plot(X, I, 'r--', alpha=0.5, lw=1, label='Infected (realtime)')
-  ax.plot(X, R, 'o', color='red', label='Recovered')
-  ax.plot(X, D, 'o', color='black', label='Dead (Disease)')
-  ax.plot(X, P, 'o', color='grey', label='Pass Away')
+  ax.plot(X, R, 'o', color='blue', label='Recovered')
+  ax.plot(X, D, 'o', color='red', label='Dead (Disease)')
 
   ax.set_xlabel('Time (days)')
   ax.set_ylabel('Population')
@@ -101,8 +97,7 @@ if __name__ == "__main__":
   print('Number of Exposed at Day ' + str(covid_params.DAYS_MODEL) + ': ' + str(E[-1]))
   print('Number of Infected at Day ' + str(covid_params.DAYS_MODEL) + ': ' + str(I[-1]))
   print('Number of Recovered at Day ' + str(covid_params.DAYS_MODEL) + ': ' + str(R[-1]))
-  print('Number of Dead due to Disease at Day ' + str(covid_params.DAYS_MODEL) + ': ' + str(D[-1]))
-  print('Number of Dead due to "natural causes" at Day ' + str(covid_params.DAYS_MODEL) + ': ' +str( P[-1]))
+  print('Number of Dead at Day ' + str(covid_params.DAYS_MODEL) + ': ' + str(D[-1]))
 
   # Display plot
   plt.show()
