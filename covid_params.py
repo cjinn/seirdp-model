f478ed4888d3132f7c6a8ab54b4e197db4f3d992
Choong Jin Ng <jin8383@gmail.com>
2020-04-20 17:00:45 -0700

    Made demo plot more readable. Adjusted params

--- covid_params.py
+++ covid_params.py
@@ -5,28 +5,24 @@ import statistics
 NUM_DAYS_IN_YEAR = 365
 
 ## Population
-POPULATION = 10000
+POPULATION = 1000
 DAYS_MODEL = 150
 
 SOCIAL_DISTANCE_RESPONSE_FACTOR = 0.5 # Factor that states how the population is willing to comply to social distancing
 POPULATION_PROPORTION_AGE_RANGE = {
-    "0-1": 0.01, 
-    "2-29": 0.19,
-    "30-59": 0.3,
-    "60-89": 0.3,
-    "89+": 0.2
+  "0-14": 0.26, 
+  "15-65": 0.65,
+  "65+": 0.09
 }
 
 ## Disease-specific
-r0 = 2.5 # https://en.wikipedia.org/wiki/Basic_reproduction_number
-r1 = 1.08  # reproduction number after quarantine measures
+r0 = 2.0 # https://en.wikipedia.org/wiki/Basic_reproduction_number
+r1 = 0.88  # reproduction number after quarantine measures
 BASE_ALPHA = 0.07 # Probability that the disease kills an infected person on a good day
 RHO_AGE_RANGE = { # Rate at which people die (1/6 = 6 days to kill a person)
-  "0-1": 0.50,
-  "2-29": 0.01,
-  "30-59": 0.2,
-  "60-89": 0.4,
-  "89+": 0.6
+  "0-14": 0.002,
+  "15-65": 0.021,
+  "65+": 0.264
 }
 RHO_AVERAGE = sum(POPULATION_PROPORTION_AGE_RANGE[ii]*RHO_AGE_RANGE[ii] 
   for ii in list(POPULATION_PROPORTION_AGE_RANGE.keys())) # Gets the average fatality rate across the different ages
@@ -36,5 +32,5 @@ SIGMA = 1.0 / (5.2 - TIME_PRESYMPTOMATIC)  # The rate at which an exposed person
 GAMMA = 1.0 / (2.0 * (4.6 - 1.0 / SIGMA))  # The rate an infectious person recovers and moves into the recovered phase. Note that for the model it only means he does not infect anybody any more.
 
 ## Disease Response
-SOCIAL_DISTANCE_THRESHOLD_POPULATION = 200 # Threshold for infected population for social distancing to take into effect
+# SOCIAL_DISTANCE_THRESHOLD_POPULATION = 0.0 # Threshold for infected population for social distancing to take into effect
 SOCIAL_DISTANCE_DAY = 57 # Day Social Distancing happened
--- seirdp.py
+++ seirdp.py
@@ -24,12 +24,12 @@ class seirdp():
     self.thresholdPrintFlag = False # When True, prints a statement of when countermeasures go into effect
 
   ## Model of SEIRDP
-  def model(self, Y, x, N, r0, thrPop, thrDay, r1, gamma, sigma, rho):
+  def model(self, Y, x, N, r0, thrDay, r1, gamma, sigma, rho):
     S, E, I, R, D = Y
 
     # Disease population hits threshold. Begin social distancing
-    if ((I + R + D) > thrPop and x < thrDay):
-      thrDay = x
+    # if ((I + R + D) > thrPop and x < thrDay):
+    #   thrDay = x
 
     # Assigning constants
     if (x >= thrDay):
@@ -59,13 +59,13 @@ class seirdp():
     return overworkedAlpha + self.baseAlpha
 
   ## Getting the populations of the different states
-  def solve(self, population, E0, thrPop, thrDay, daysModel):
+  def solve(self, population, E0, thrDay, daysModel):
     X = np.arange(daysModel)  # time steps array
     N0 = population - E0, E0, 0, 0, 0  # S, E, I, R at initial step
     self.thresholdPrintFlag = True
     
     y_data_var = scipy.integrate.odeint(self.model, N0, X, args=(
-      population, self.r0, thrPop, thrDay, self.r1, self.gamma, self.sigma, self.rho))
+      population, self.r0, thrDay, self.r1, self.gamma, self.sigma, self.rho))
       
     S, E, I, R, D = y_data_var.T  # transpose and unpack
     return X, S, E, I, R, D  # note these are all arrays
@@ -73,31 +73,36 @@ class seirdp():
 ## If this is the main python file...
 if __name__ == "__main__":
   E0 = 1  # exposed at initial time step
+  N = covid_params.POPULATION
 
   modelInstance = seirdp(covid_params.r0, covid_params.r1, covid_params.GAMMA, covid_params.SIGMA,
     covid_params.BASE_ALPHA, covid_params.RHO_AVERAGE, covid_params.SOCIAL_DISTANCE_RESPONSE_FACTOR)
-  X, S, E, I, R, D = modelInstance.solve(covid_params.POPULATION, E0, covid_params.SOCIAL_DISTANCE_THRESHOLD_POPULATION, covid_params.SOCIAL_DISTANCE_DAY, covid_params.DAYS_MODEL)
+  X, S, E, I, R, D = modelInstance.solve(N, E0, covid_params.SOCIAL_DISTANCE_DAY, covid_params.DAYS_MODEL)
 
-  # Plot
+  # Plot percentages of Population
   fig = plt.figure(dpi=75, figsize=(20,16))
   ax = fig.add_subplot(111)
-  ax.plot(X, S, 'o', color='orange', label='Susceptible')
-  ax.plot(X, E, 'y', alpha=0.5, lw=2, label='Exposed (realtime)')
-  ax.plot(X, I, 'r--', alpha=0.5, lw=1, label='Infected (realtime)')
-  ax.plot(X, R, 'o', color='blue', label='Recovered')
-  ax.plot(X, D, 'o', color='red', label='Dead (Disease)')
+  ax.plot(X, S/N*100, 'o', color='green', label='Susceptible')
+  ax.plot(X, E/N*100, 'o', color='yellow', label='Exposed (realtime)')
+  ax.plot(X, I/N*100, 'o', color='red', label='Infected (realtime)')
+  ax.plot(X, R/N*100, 'o', color='blue', label='Recovered')
+  ax.plot(X, D/N*100, '-', color='grey', label='Dead (Disease)')
 
+  # Plot threshold day
+  plt.axvline(x=covid_params.SOCIAL_DISTANCE_DAY, color='black')
+
+  # Prepare plot for viewing
   ax.set_xlabel('Time (days)')
-  ax.set_ylabel('Population')
-  ax.set_ylim(bottom=1.0)
-  legend = ax.legend(title='SEIRDP model')
+  ax.set_ylabel('% Of Population' + str(N))
+  ax.set_ylim(bottom=0.0, top=100.0)
+  legend = ax.legend(title='SEIRD model')
 
   # Print numbers
-  print('Number of Susceptible at Day ' + str(covid_params.DAYS_MODEL) + ': ' + str(S[-1]))
-  print('Number of Exposed at Day ' + str(covid_params.DAYS_MODEL) + ': ' + str(E[-1]))
-  print('Number of Infected at Day ' + str(covid_params.DAYS_MODEL) + ': ' + str(I[-1]))
-  print('Number of Recovered at Day ' + str(covid_params.DAYS_MODEL) + ': ' + str(R[-1]))
-  print('Number of Dead at Day ' + str(covid_params.DAYS_MODEL) + ': ' + str(D[-1]))
+  print('Number of Susceptible at Day ' + str(covid_params.DAYS_MODEL) + ': ' + str(round(S[-1])))
+  print('Number of Exposed at Day ' + str(covid_params.DAYS_MODEL) + ': ' + str(round(E[-1])))
+  print('Number of Infected at Day ' + str(covid_params.DAYS_MODEL) + ': ' + str(round(I[-1])))
+  print('Number of Recovered at Day ' + str(covid_params.DAYS_MODEL) + ': ' + str(round(R[-1])))
+  print('Number of Dead at Day ' + str(covid_params.DAYS_MODEL) + ': ' + str(round(D[-1])))
 
   # Display plot
   plt.show()
